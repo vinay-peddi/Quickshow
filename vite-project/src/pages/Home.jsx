9b24fd4f6e8e5446d6c68d44b0151b172d7d173c
vinay <vinaynaiudu@gmail.com>
2025-06-24 14:03:46 +0530

    first commit

---
+++ vite-project/src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import HeroSection from '../components/HeroSection';
+import FeaturedSection from '../components/FeaturedSection';
+import TrailersSection from '../components/TrailersSection';
+
+const Home = () => {
+    return (
+        <>
+        <HeroSection/>
+        <FeaturedSection/>
+        <TrailersSection/>
+            
+        </>
+    );
+};
+
+export default Home;
